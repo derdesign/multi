d239916275040a3d2ff848d1ae069e8e33b4f43d
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-20 11:16:54 -0400

    Finished batch 2 of async tests

--- test/async.js
+++ test/async.js
@@ -6,7 +6,7 @@ var vows = require('vows'),
     Multi = require('../'),
     EventEmitter = require('events').EventEmitter;
     
-vows.describe('Asynchronous execution').addBatch({
+vows.describe('Asynchronous execution')/*.addBatch({
   
   'Running with successful callbacks': {
     
@@ -48,12 +48,59 @@ vows.describe('Asynchronous execution').addBatch({
     }
     
   }
+})*/.addBatch({
+  
+  'Running with errors': {
+    
+    topic: function() {
+      var promise = new EventEmitter(),
+          order = [],
+          multi = new Multi(context, {parallel: false, interrupt: false});
+      multi.sleep(1);
+      multi.sleep(2);
+      multi.error(3);
+      multi.randSleep(order);
+      multi.randSleep(order);
+      multi.exec(function(err, results) {
+        promise.emit('success', {err: err, results: results, order: order});
+      });
+      return promise;
+    },
+    
+    'Errors should be an array': function(topic) {
+      assert.isArray(topic.err);
+    },
+    
+    'Results should be an array': function(topic) {
+      assert.isArray(topic.results);
+    },
+    
+    'Errors.length should match method calls': function(topic) {
+      assert.equal(topic.err.length, 5);
+    },
+    
+    'Results.length should match method calls': function(topic) {
+      assert.equal(topic.results.length, 5);
+    },
+    
+    'Successful callbacks should report null as error': function(topic) {
+      assert.isNull(topic.err[0]);
+    },
+    
+    'The reported error matches the actual error': function(topic) {
+      var err = topic.err[2];
+      assert.isTrue(err instanceof Error && err.toString() == 'Error: The Error');
+    },
+    
+    'Results are correctly reported': function(topic) {
+      // Errored callbacks report null
+      var expected = [1, 2, null].concat(topic.order);
+      assert.deepEqual(expected, topic.results);
+    }
+    
+  }
+  
 }).export(module);
-
-// Running with errors
-// - An array of errors should be reported
-// - The reported error matches the actual error
-// - The Errors array length should match method calls
 
 // Running with interrupt on error
 // - Errors & Results should be arrays
