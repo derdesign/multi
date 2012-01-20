500a35caf4916df1abbf1ec2ac8fb4265b124572
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-20 12:34:01 -0400

    Finished examples

--- examples/multi-parallel.js
+++ examples/multi-parallel.js
@@ -1,40 +1,9 @@
 
 /* Multi parallel */
 
-var util = require('util');
+var util = require('util'),
+    context = require('../test/fixtures/context'),
     Multi = require('../');
-
-var context = {
-  
-  // Runs a callback after a specific delay
-  sleep: function(delay, callback) {
-    setTimeout(function() {
-      callback(null, delay);
-    }, delay);
-  },
-  
-  // Runs a callback at a random time
-  randSleep: function(arr, callback) {
-    var t = Math.ceil(Math.random()*10);
-    if (util.isArray(arr)) arr.push(t);
-    setTimeout(function() {
-      callback(null, t);
-    }, t);
-  },
-  
-  // Callback returning error
-  error: function(timeout, callback) {
-    setTimeout(function() {
-      callback(new Error('The Error'), null);
-    }, timeout);
-  },
-  
-  // Callback returning a result
-  sum: function(a, b, callback) {
-    callback(null, a+b);
-  }
-  
-}
 
 var order = [],  // Array containing the order of execution
     multi = new Multi(context, {parallel: true});
