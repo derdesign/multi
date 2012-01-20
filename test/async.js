772313066ce4ffcd34eef02fc7da95c0661a25f7
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-20 10:59:24 -0400

    Finished batch 1 of async tests

--- test/async.js
+++ test/async.js
@@ -11,31 +11,44 @@ vows.describe('Asynchronous execution').addBatch({
   'Running with successful callbacks': {
     
     topic: function() {
-      
-    },
-    
-    'Callbacks run sequentially': function(topic) {
-      
+      var promise = new EventEmitter(),
+          order = [],
+          multi = new Multi(context, {parallel: false, interrupt: false});
+      multi.randSleep(order);
+      multi.randSleep(order);
+      multi.randSleep(order);
+      multi.sum(2,3);
+      multi.exec(function(err, results) {
+        promise.emit('success', {err: err, results: results, order: order});
+      });
+      return promise;
     },
     
     'Results should be an array': function(topic) {
-      
-    },
-    
-    'Results are pushed in order of completion': function(topic) {
-      
-    },
-    
-    'Results length should match method calls': function(topic) {
-      
+      assert.isArray(topic.results);
     },
     
     'No errors should be returned': function(topic) {
-      
+      assert.isNull(topic.err);
+    },
+    
+    'Callbacks run sequentially': function(topic) {
+      var expected = topic.results.slice(0,3);
+      assert.deepEqual(topic.order, expected);
+    },
+    
+    'Results are pushed in order of completion': function(topic) {
+      var expected = [].concat(topic.order);
+      expected.push(5);
+      assert.deepEqual(expected, topic.results);
+    },
+    
+    'Results length should match method calls': function(topic) {
+      assert.equal(topic.results.length, 4);
     }
     
   }
-});
+}).export(module);
 
 // Running with errors
 // - An array of errors should be reported
