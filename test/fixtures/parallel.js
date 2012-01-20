70d07563fce428576329067d1183fc77bc9106c0
Ernesto Mendez <der.kunstler@gmail.com>
2012-01-19 21:03:33 -0400

    Update

--- test/fixtures/parallel.js
+++ test/fixtures/parallel.js
@@ -1,7 +1,39 @@
 
 function parallelTest(promise) {
   
-  promise.emit('success', true);
+  var Multi = require('../../'),
+      runOrder = [], 
+      finishOrder = [];
+  
+  var randFunc = function(callback) {
+    var t = Math.ceil(Math.random()*2000);
+    runOrder.push(t);
+    setTimeout(function() {
+      finishOrder.push(t);
+    }, t);
+  }
+  
+  var context = {
+    method1: randFunc,
+    method2: randFunc,
+    method3: randFunc,
+    method4: randFunc
+  }
+  
+  var multi = new Multi(context, {parallel: true});
+  
+  multi.method1();
+  multi.method2();
+  multi.method3();
+  multi.method4();
+  
+  // multi.exec(function(err, results) {
+  //   promise.emit('success', true);
+  // });
+  
+  setTimeout(function() {
+    promise.emit('success', true);
+  }, 3000);
   
 }
 
